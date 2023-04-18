9f07d92270d2c6a71e4a2517a8e8c3230590be97
Cardboardface <kodarit.torstai@gmail.com>
2023-04-18 19:33:26 +0300

    list loppuun

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -4,6 +4,18 @@ import { useState } from "react";
 export const List = () => {
 	const [content, setContent] = useState([]);
 	const [inputValue, setInputValue] = useState('');
+
+	function clearList(){
+		setContent([]);
+	}
+
+	const removeHandler = (removeIndex) => {
+		const removeItem = content.filter((item, index) => {
+			return removeIndex !== index;
+		});
+		setContent(removeItem);
+	}
+	
 	return (
 		<div>
 			<div>
@@ -13,9 +25,24 @@ export const List = () => {
 					setContent(temp);
 					setInputValue('');
 				}}>
-					<input type="text" value={inputValue} required/>
+					<input 
+						type="text" 
+						value={inputValue}
+						onChange={e => setInputValue(e.target.value)}
+						required
+					/>
 					<input type="submit" value="Submit"/>
+					<ul>
+						{
+							content.map((item, index) => (
+							<li>
+								<p key={index}>{item}</p>
+								<button onClick={() => removeHandler(index)} type="button">Remove</button>
+							</li>))
+						}
+					</ul>
 				</form>
+				<button onClick={clearList}>Clear list</button>
 			</div>
 		</div>
 	);
